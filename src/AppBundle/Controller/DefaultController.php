14f5c1c8da9522da7da46d74b8bbe8e9e2240a9c
rwachtler <Brie138!Goas>
2015-11-12 13:11:12 +0100

    Installed necessary resources (jquery, bootstrap)
    + set up the basic index template
    + added basic styling

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,8 +13,10 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
+        //TODO: Load available extensions and render them inside template
+
+        // return the default temlate
+        return $this->render('main/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
